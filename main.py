8dc72d66edc6d358320780d6dd6cb452f5c58e07
Riley Martinez <riley.m.martinez@gmail.com>
2020-07-04 23:46:51 -0500

    Implement ffmpeg module and mux function
    Overhauled main and utils script to include higher resolution streams using ffmpeg to combine audio and video

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import utils
 
-
 # Main method
 def main():
     url = utils.get_url()
     output_path = utils.get_output_path()
     audio_only = utils.get_audio_only()
-    stream_list = utils.list_streams(url, audio_only)
-    if len(stream_list) == 0:
-        print('There are no streams available for the options that you specified.')
-        return
-    itag = utils.get_itag()
-    utils.download_video(url, itag, audio_only, output_path)
-
+    if not audio_only:
+        video_quality = utils.get_stream_quality()
+    if audio_only:
+        stream_list = utils.list_streams(url, audio_only)
+        if len(stream_list) == 0:
+            print('There are no streams available for the options that you specified.')
+            return
+        itag = utils.get_itag()
+        utils.download_video(url, itag, audio_only, output_path)
+        utils.print_video_statistics(url)
+    elif video_quality == 1:
+        progressive = True
+        stream_list = utils.list_streams(url, audio_only, progressive)
+        if len(stream_list) == 0:
+            print('There are no streams available for the options that you specified.')
+            return
+        itag = utils.get_itag()
+        utils.download_video(url, itag, audio_only, output_path)
+        utils.print_video_statistics(url)
+    else:
+        progressive = False
+        stream_lists = utils.list_streams(url, audio_only, progressive)
+        if not stream_lists[0]:
+            print('There are no video streams available for the options that you specified.')
+            return
+        elif not stream_lists[1]:
+            print('There are no audio streams available for the options that you specified.')
+            return
+        elif not stream_lists:
+            print('There are no video or audio streams available for the options that you specified.')
+            return
+        itags = utils.get_dash_itags()
+        utils.mux(url, itags[0], itags[1], output_path)
 
 # Run main
 user_input = ''
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 from pytube.exceptions import *
+import ffmpeg
 import tkinter.filedialog
 from datetime import datetime
 import os
@@ -36,7 +37,7 @@ def get_output_path():
         elif browse != 'y' and browse != 'n':
             print('Invalid entry.')
         else:
-            print('File will be saved to the default location within the current working directory (videos/audio).')
+            print('File will be saved to the default location within the current working directory (video/audio).')
             break
 
 
@@ -54,26 +55,42 @@ def get_audio_only():
 
 
 # List available streams based on users input
-def list_streams(url, audio_only=False):
+def list_streams(url, audio_only=False, progressive=False):
     yt = YouTube(url)
     print('Available streams for {} based on your input:\n'.format(yt.title))
     if audio_only:
-        audio_stream_list = yt.streams.filter(only_audio=audio_only).order_by('resolution').desc()
-        for stream in audio_stream_list:
-            print('ITAG: {}, File Type: {}, Codec: {}, '
-                  'Average Bitrate: {}, File Size: {:.2f}MB'.format(stream.itag, stream.subtype,
-                                                            stream.audio_codec, stream.abr, stream.filesize * 1e-6))
+        audio_stream_list = yt.streams.filter(only_audio=audio_only).order_by('bitrate').desc()
+        print_audio_streams(audio_stream_list)
         return audio_stream_list
-    else:
-        video_stream_list = yt.streams.filter(progressive=True).order_by('bitrate').desc()
-        for stream in video_stream_list:
-            print('ITAG: {}, File Type: {}, Video Codec: {}, '
-                  'Audio Codec: {}, Resolution: {}, '
-                  'Framerate: {}, File Size: {:.2f}MB'.format(stream.itag, stream.subtype,
-                                                        stream.video_codec, stream.audio_codec,
-                                                        stream.resolution, stream.fps, stream.filesize * 1e-6))
+    elif progressive:
+        video_stream_list = yt.streams.filter(progressive=True).order_by('resolution').desc()
+        print_video_streams(video_stream_list)
         return video_stream_list
+    else:
+        video_stream_list = yt.streams.filter(adaptive=True, only_video=True).order_by('subtype').desc()
+        audio_stream_list = yt.streams.filter(adaptive=True, only_audio=True).order_by('subtype').desc()
+        print('--------------------Video Streams--------------------')
+        print_video_streams(video_stream_list)
+        print('\n--------------------Audio Streams--------------------')
+        print_audio_streams(audio_stream_list)
+        return video_stream_list, audio_stream_list
 
+
+def print_audio_streams(stream_list):
+    for stream in stream_list:
+        print('ITAG: {}, File Type: {}, Codec: {}, '
+              'Average Bitrate: {}, File Size: {:.2f}MB'.format(stream.itag, stream.subtype,
+                                                                stream.audio_codec, stream.abr,
+                                                                stream.filesize * 1e-6))
+
+
+def print_video_streams(stream_list):
+    for stream in stream_list:
+        print('ITAG: {}, File Type: {}, Video Codec: {}, '
+              'Audio Codec: {}, Resolution: {}, '
+              'Framerate: {}, File Size: {:.2f}MB'.format(stream.itag, stream.subtype,
+                                                          stream.video_codec, stream.audio_codec,
+                                                          stream.resolution, stream.fps, stream.filesize * 1e-6))
 
 # Prompt user for ITAG ID and return the ID converted to an int.
 def get_itag():
@@ -85,16 +102,34 @@ def get_itag():
             print('Invalid ITAG ID.')
 
 
-# Downloads youtube video specified by itag id
-def download_video(url, itag, audio_only, output_path=None):
-    yt = YouTube(url)
-    stream = yt.streams.get_by_itag(itag)
-    yt.register_on_progress_callback(download_progress)
+def get_dash_itags():
+    while True:
+        try:
+            video_itag = int(input('\nEnter an ITAG ID from the video stream list: '))
+            audio_itag = int(input('Enter an ITAG ID from the audio stream list: '))
+            return video_itag, audio_itag
+        except ValueError as e:
+            print('Invalid ITAG ID:', e)
 
-    print('\nDownload Started - {}'.format(yt.title))
-    now = datetime.now()
-    dl_string = now.strftime("%m%d%Y_%H%M%S")
-    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+
+# Prompt user for either DASH (high quality) or Progressive (lower quality and combined audio/video) streams.
+def get_stream_quality():
+    while True:
+        user_input = int(input('Video Quality Select:\n\n[1] Progressive (<= 720p)\n[2] Adaptive (>= 1080p)\n\n'
+                               'Enter selection: '))
+        if user_input == 1 or user_input == 2:
+            return user_input
+        else:
+            print('Invalid entry.')
+
+
+# Downloads youtube video specified by itag id
+def download_video(url, itag, audio_only=False, output_path=None):
+    yt = YouTube(url, on_progress_callback=download_progress)
+    stream = yt.streams.get_by_itag(itag)
+    video_title = yt.player_response.get("videoDetails", {}).get("title")
+    print('\nDownload Started - {}'.format(video_title))
+    dl_string = datetime.now().strftime("%m%d%Y_%H%M%S")
 
     if output_path is None:
         if os.name == 'nt':
@@ -105,15 +140,57 @@ def download_video(url, itag, audio_only, output_path=None):
             folder = 'audio'
         else:
             folder = 'video'
-        stream.download(output_path=f'./{folder}', filename_prefix=f'{dl_string}_')
+        stream.download(output_path=f'./{folder}', filename_prefix=f'{dl_string}_', filename=video_title)
         print('\nDownload Completed - Saved to {}'.format(os.getcwd() + dir_sep + folder))
     else:
-        stream.download(output_path=output_path, filename_prefix=f'{dl_string}_')
+        stream.download(output_path=output_path, filename_prefix=f'{dl_string}_', filename=video_title)
         print('\nDownload Completed - Saved to {}'.format(output_path))
 
+
+def mux(url, video_itag, audio_itag, output_path=None):
+    yt = YouTube(url, on_progress_callback=download_progress)
+    video_stream = yt.streams.get_by_itag(video_itag)
+    audio_stream = yt.streams.get_by_itag(audio_itag)
+    video_type = video_stream.subtype
+    audio_type = audio_stream.subtype
+    dl_string = datetime.now().strftime("%m%d%Y_%H%M%S")
+
+    print('\nDownloading Video Stream...')
+    video_stream.download(output_path='./temp', filename='video')
+    print('Downloading Audio Stream...')
+    audio_stream.download(output_path='./temp', filename='audio')
+
+    if os.name == 'nt':
+        dir_sep = '\\'
+    else:
+        dir_sep = '/'
+
+    if output_path is None:
+        file_path = f'{os.getcwd()}{dir_sep}video{dir_sep}{dl_string}_{video_stream.default_filename}'
+        if not os.path.exists('./video'):
+            os.makedirs('./video')
+    else:
+        file_path = f'{output_path}{dir_sep}{dl_string}_{video_stream.default_filename}'
+
+    video_stream = ffmpeg.input(f'./temp{dir_sep}video.{video_type}')
+    audio_stream = ffmpeg.input(f'./temp{dir_sep}audio.{audio_type}')
+
+    ffmpeg.output(video_stream, audio_stream, file_path).run(overwrite_output=True)
+
+    os.remove(f'./temp{dir_sep}video.{video_type}')
+    os.remove(f'./temp{dir_sep}audio.{audio_type}')
+
+    print_video_statistics(url)
+
+def print_video_statistics(url):
+    yt = YouTube(url)
+    video_title = yt.player_response.get("videoDetails", {}).get("title")
+    video_author = yt.player_response.get("videoDetails", {}).get("author", "unknown")
+    dt_string = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+
     print("\nYou just downloaded",
-          bcolors.OKBLUE + yt.player_response.get("videoDetails", {}).get("title") + bcolors.ENDC,
-          "by", bcolors.OKBLUE + yt.player_response.get("videoDetails", {}).get("author", "unknown") + bcolors.ENDC)
+          bcolors.OKBLUE + video_title + bcolors.ENDC,
+          "by", bcolors.OKBLUE + video_author + bcolors.ENDC)
 
     print("This video has",
           bcolors.OKBLUE + "{:,}".format(
@@ -133,7 +210,7 @@ def download_progress(stream, chunk, bytes_remaining):
     percent_complete = '{0:.1f}'.format(current_progress * 100)
     filled_length = int(bar_length * current_progress)
     bar = fill * filled_length + '-' * (bar_length - filled_length)
-    print(f'\r{prefix} |{bar}| {percent_complete}% {suffix}', end=print_end)
+    print(f'{prefix} |{bar}| {percent_complete}% {suffix}', end=print_end)
 
 
 # Class that contains color attribute constants for displayed video stats in download_video()
@@ -146,3 +223,4 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
